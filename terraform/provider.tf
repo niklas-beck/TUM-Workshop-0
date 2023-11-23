3eca6ba630081b6f06e33d6ea803d96e12a60208
Niklas Beck <Niklas.Beck@berylls.com>
2023-11-23 09:53:37 +0100

    change tf state storage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "terraform-statefile"
-    storage_account_name = "terraformstorage543"
+    resource_group_name  = var.resource_group_name
+    storage_account_name = "tfstoragetumworkshop"
     container_name       = "tum-workshop-session1"
-    key                  = "tumworkshop-0.tfstate"
+    key                  = "repo-0.tfstate"
     use_oidc             = true
   }
 
