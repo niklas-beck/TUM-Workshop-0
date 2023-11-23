ba633e44da61bb3f2763b5ef1249a05bcc0fc434
Niklas Beck <Niklas.Beck@berylls.com>
2023-11-23 09:42:07 +0100

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,8 @@
-#Set the terraform required version
 terraform {
-  required_version = "~> 1.6.4"
   required_providers {
-    # It is recommended to pin to a given version of the Provider
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.81.0"
-    }
-
-    random = {
-      version = "~>2"
+      version = ">= 3.7.0"
     }
   }
 
@@ -18,15 +11,12 @@ terraform {
     storage_account_name = "terraformstorage543"
     container_name       = "tum-workshop-session1"
     key                  = "tumworkshop-0.tfstate"
+    use_oidc             = true
   }
 
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
-
-# Data
-
-# Make client_id, tenant_id, subscription_id and object_id variables
-#data "azurerm_client_config" "current" {}
